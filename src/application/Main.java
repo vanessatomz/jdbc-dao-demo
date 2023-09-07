fc864f7c10b4136457b55c811abc220ce133f5ee
Vanessa Tomaz <vanessatomaz246@gmail.com>
2023-09-07 16:21:31 -0300

    Department entity class

---
+++ src/application/Main.java
@@ -0,0 +1,10 @@
+package application;
+
+import model.Department;
+
+public class Main {
+    public static void main(String[] args) {
+        Department department = new Department(1,"Electronics");
+        System.out.println(department);
+    }
+}
