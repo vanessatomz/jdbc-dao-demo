36cc8d2518672facfa9f9266907cee027ca35b3a
Vanessa Tomaz <vanessatomaz246@gmail.com>
2023-09-07 16:39:57 -0300

    Seller entity class

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,20 @@
 package application;
 
 import model.Department;
+import model.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
+
         Department department = new Department(1,"Electronics");
         System.out.println(department);
+
+        Seller seller = new Seller(1,"Bob","bob@gmai.com",
+                                    new Date(),3000.0,department);
+        System.out.println(seller);
+
+
     }
 }
